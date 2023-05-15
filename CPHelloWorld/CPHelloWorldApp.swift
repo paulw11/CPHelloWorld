a84e5815c8e9700e4b055c4c3f0aa3e5ed613f01
Paul Wilkinson <paulw@wilko.me>
2023-05-16 09:54:44 +1000

    Initial import

---
+++ CPHelloWorld/CPHelloWorldApp.swift
@@ -0,0 +1,20 @@
+//
+//  CPHelloWorldApp.swift
+//  CPHelloWorld
+//
+//  Created by Paul Wilkinson on 16/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct CPHelloWorldApp: App {
+    
+
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CPHelloWorld/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CPHelloWorld
+//
+//  Created by Paul Wilkinson on 16/5/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
